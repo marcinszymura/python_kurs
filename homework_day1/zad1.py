f8d6033f868f8843a0c598252b1e06e681a505c9
Marcin <mszymura@crontech.com.pl>
2020-03-25 19:23:00 +0100

    first commit

---
+++ homework_day1/zad1.py
@@ -0,0 +1,14 @@
+# ### Zadanie 1.1 | Interakcja i proste obliczenia (ok. 2 godz.)
+# ​
+# Napisz program, który prosi użytkownika (przez `input()`), żeby podał, ile kosztuje kilo ziemniaków. Niech program
+# policzy i wyświetli, ile trzeba będzie zapłacić za pięć kilo ziemniaków.
+# ​
+# Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał,
+# ile kosztuje kilo ziemniaków i ile kilo chce kupić.
+# Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki.
+# ​
+# Potem napisz program, który prosi użytkownika (przez `input()`),
+# żeby podał, ile kosztuje kilo ziemniaków, ile kilo ziemniaków chce kupić,
+# ile kosztuje kilo bananów i ile kilo bananów chce kupić.
+# Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki i banany razem.
+# I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za banany czy za ziemniaki.
