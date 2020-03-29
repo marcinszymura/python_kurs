01761b18be609b6e9ed7f5f9e35bd4c68a9b480a
Marcin <mszymura@crontech.com.pl>
2020-03-29 13:53:29 +0200

    zad13_update

--- day2/zad13.py
+++ day2/zad13.py
@@ -1,8 +1,7 @@
-
-counter = 1
+counter = 0
 week_temperature = 0
-while counter < 8:
+while counter < 7:
+    counter += 1
     day_temperature = float(input(f'podaj temperature dla dnia {counter}: '))
     week_temperature = week_temperature + day_temperature
-    counter += 1
-print(f'sr tempetatura = {float(week_temperature / 7 )}')
+print(f'sr tempetatura = {float(week_temperature / counter)}')
