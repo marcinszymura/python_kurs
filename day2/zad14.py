ab689c0e3de0ca2f02642e310f84a08a2d8269f2
Marcin <mszymura@crontech.com.pl>
2020-03-29 14:48:36 +0200

    zad13_update

--- day2/zad14.py
+++ day2/zad14.py
@@ -7,12 +7,10 @@ while True:
     x = input('Podaj liczbe lun naciśnij e aby zakończyć: ')
     if x == 'e':
         break
-    if min_number is None:
+    x = float(x)
+    if min_number is None or x < min_number:
         min_number = x
-    if max_number is None:
+    if max_number is None or x > max_number:
         max_number = x
-    if x < min_number:
-        min_number = x
-    if x > max_number:
-        max_number = x
+
 print(f'min: {float(min_number):.2f} max: {float(max_number):.2f}')
