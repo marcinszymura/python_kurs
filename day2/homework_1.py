c81602e36146c475445958a4f6eb3f3adfab0c16
Marcin <mszymura@crontech.com.pl>
2020-04-12 16:00:18 +0200

    homework1 update

--- day2/homework_1.py
+++ day2/homework_1.py
@@ -2,23 +2,26 @@ x = int(input('podaj pozycje dla x: '))
 y = int(input('podaj pozycje dla y: '))
 result = ''
 
-if (0 > x > 100) or (0 > y > 100):
+if (x > 100) or (x < 0) or (y > 100) or (y < 0):
     result = 'poza planszą'
-elif (0 <= x <= 10) and (0 <= y <= 10):
-    result = 'w lewym dolnym rogu'
-elif (0 <= x <= 10) and (91 <= y <= 100):
-    result = 'w lewym górnym rogu'
-elif (91 <= x <= 100) and (0 <= y <= 10):
-    result = 'w prawym dolnym rogu'
-elif (91 <= x <= 100) and (91 <= y <= 100):
-    result = 'w prawym górnym rogu'
-elif (0 <= x <= 10) and (11 <= y <= 90):
-    result = 'na lewej krawędzi'
-elif (10 <= x <= 90) and (0 <= y <= 10):
+elif x <= 10:
+    if y <= 10:
+        result = 'w lewym dolnym rogu'
+    elif y >= 90:
+        result = 'w lewym górnym rogu'
+    else:
+        result = 'na lewej krawędzi'
+elif x >= 91:
+    if y <= 10:
+        result = 'w prawym dolnym rogu'
+    elif y >= 90:
+        result = 'w prawym górnym rogu'
+    else:
+        result = 'na prawej krawędzi'
+elif y < 10:
     result = 'na dolniej krawędzi'
-elif (91 <= x <= 100) and (11 <= y <= 90):
-    result = 'na prawej krawędzi'
-elif (91 <= x <= 100) and (11 <= y <= 90):
+elif y > 90:
+    result = 'na górnej krawędzi'
     result = 'na górnej krawędzi'
 else:
     result = 'w centrum'
