443fe10293cfb7b0b5202217f85808d634cfbeaa
Marcin <mszymura@crontech.com.pl>
2020-05-09 09:16:46 +0200

    Odp 2.

--- sprawdzan/spr_01.py
+++ sprawdzan/spr_01.py
@@ -1,3 +1,8 @@
 # 1. Wymień poznane dotąd struktury danych:
 # lista, słownik, tuple
 # int, float, bool, string
+
+
+# 2. Które z nich są mutowalne a które nie?:
+# mutowalne: listy, słowniki
+# niemutowalne: tuple2, int, float, bool, string
