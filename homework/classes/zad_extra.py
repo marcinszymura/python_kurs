13c53d558eb782bfd87608c531e0aa3a6d41f4e8
Marcin <mszymura@crontech.com.pl>
2020-05-22 18:24:59 +0200

    zadania extra poprawki

--- homework/classes/zad_extra.py
+++ homework/classes/zad_extra.py
@@ -8,7 +8,14 @@
 #
 # [4, 5, 6]
 
-def przytnij(data, start, stop):
+def przytnij(data: list, start: str, stop: str) -> list:
+    """
+    Funkcja do przycinania listy
+    :param data: lista
+    :param start: poczatek listy
+    :param stop: koniec listy
+    :return:
+    """
     first = list(filter(start, data))
     last = list(filter(stop, data))
     return [a for a in first if a <= last[0]]
@@ -20,6 +27,8 @@ print(przytnij(
     stop=lambda x: x == 6))
 
 print('*' * 50)
+
+
 # Zaimplementuj zestaw dekoratorów otaczających zwracany przez funkcję napis tagami HTML (pogrubienie, podkreślenie, przekreślenie):
 #
 # Przykład użycia:
@@ -29,26 +38,53 @@ print('*' * 50)
 #     return f'To jest {arg}'
 
 def bold(func):
+    """
+
+    :param func:
+    :return:
+    """
+
     def add_bold(*args, **kwargs):
         return '<b>' + func(*args, **kwargs) + '</b>'
+
     return add_bold
 
 
 def italic(func):
+    """
+
+    :param func:
+    :return:
+    """
+
     def add_italic(*args, **kwargs):
         return '<i>' + func(*args, **kwargs) + '</i>'
+
     return add_italic
 
 
 def underline(func):
+    """
+
+    :param func:
+    :return:
+    """
+
     def add_underline(*args, **kwargs):
         return '<u>' + func(*args, **kwargs) + '</u>'
+
     return add_underline
+
 
 @underline
 @italic
 @bold
-def foo(arg):
+def foo(arg: str) -> str:
+    """
+    przykładowa funkcja
+    :param arg:
+    :return:
+    """
     return f'To jest {arg}'
 
 
@@ -67,16 +103,29 @@ import time
 
 
 def log(func):
+    """
+    funkcja do mierzenia czasu wykonani adanej dunkcji
+    :param func:
+    :return:
+    """
+
     def add_log(*args, **kwargs):
         start = time.time()
         func(*args, **kwargs)
         stop = time.time()
         return f'{func.__name__}: czas wykonania: {stop - start:.2f}'
+
     return add_log
 
 
 @log
-def foo(arg):
+def foo(arg: str) -> str:
+    """
+    przykładowa funkcja
+    :param arg:
+    :return:
+    """
     return f'To jest {arg}'
+
 
 print(foo('logowanie'))
