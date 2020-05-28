a304e3ee1c4556df68c7bcb2d99a0d29d0e0b0ef
MSzymur <mszymura@crontech.com.pl>
2020-05-28 15:33:57 +0200

    homework1 wersja3

--- homework/files/policz_slowa_w_txt.py
+++ homework/files/policz_slowa_w_txt.py
@@ -4,58 +4,58 @@
 # ​
 # Niech program dla podanej nazwy pliku tekstowego i dla podanego słowa policzy ile razy
 # to słowo występuje w pliku (np. Tadeusz w pliku `pan-tadeusz.txt`).
-
-plik_txt = 'pan-tadeusz.txt'
-slowo = 'Tadeusz'
-
-
-def policz_ile(plik_txt: str, slowo: str) ->str:
-    """
-    funkcja do liczenia wystąpień danego słowa w pliku txt
-    :param plik_txt: nazwa pliku
-    :param slowo: słowo do wyszukania
-    :return:
-    """
-    with open(plik_txt, 'r', encoding="utf8") as plik:
-        w = plik.read()
-    return f'słowo "{slowo}" wystąpiło {w.count(slowo)} w pliku {plik_txt}'
-
-
-print(policz_ile('pan-tadeusz.txt', 'Tadeusz'))
-
-
-
-print('*' * 50)
-### Zadanie 6.2 | Policz wybrane słowo (1 godz.)
-# ​
-# Plik z utworem "Pan Tadeusz" do pobrania: http://pgradzinski.students.alx.pl/kpython/pan-tadeusz.txt
-# ​
-# Niech program dla podanej nazwy pliku tekstowego i dla podanego słowa policzy ile razy to słowo występuje w
-# pliku (np. Tadeusz w pliku `pan-tadeusz.txt`).
-
-
-import re
-
-
-def policz_slowa(plik_txt: str) -> str:
-    with open(plik_txt, 'r', encoding="utf8") as plik:
-        c = 1
-        counter = dict()
-        while True:
-            line = plik.readline()
-            if not line:
-                break
-            for word in line.split(' '):
-                word = re.sub(r"\W", "", word)
-                counter[word.lower()] = counter.get(word.lower(), 0) + 1
-            c += 1
-
-    for word, count in sorted(counter.items(), key=lambda kv: kv[1], reverse=True):
-        print(f'{word} -> {count}')
-
-
-policz_slowa('pan-tadeusz.txt')
-
+#
+# plik_txt = 'pan-tadeusz.txt'
+# slowo = 'Tadeusz'
+#
+#
+# def policz_ile(plik_txt: str, slowo: str) ->str:
+#     """
+#     funkcja do liczenia wystąpień danego słowa w pliku txt
+#     :param plik_txt: nazwa pliku
+#     :param slowo: słowo do wyszukania
+#     :return:
+#     """
+#     with open(plik_txt, 'r', encoding="utf8") as plik:
+#         w = plik.read()
+#     return f'słowo "{slowo}" wystąpiło {w.count(slowo)} w pliku {plik_txt}'
+#
+#
+# print(policz_ile('pan-tadeusz.txt', 'Tadeusz'))
+#
+#
+#
+# print('*' * 50)
+# ### Zadanie 6.2 | Policz wybrane słowo (1 godz.)
+# # ​
+# # Plik z utworem "Pan Tadeusz" do pobrania: http://pgradzinski.students.alx.pl/kpython/pan-tadeusz.txt
+# # ​
+# # Niech program dla podanej nazwy pliku tekstowego i dla podanego słowa policzy ile razy to słowo występuje w
+# # pliku (np. Tadeusz w pliku `pan-tadeusz.txt`).
+#
+#
+# import re
+#
+#
+# def policz_slowa(plik_txt: str) -> str:
+#     with open(plik_txt, 'r', encoding="utf8") as plik:
+#         c = 1
+#         counter = dict()
+#         while True:
+#             line = plik.readline()
+#             if not line:
+#                 break
+#             for word in line.split(' '):
+#                 word = re.sub(r"\W", "", word)
+#                 counter[word.lower()] = counter.get(word.lower(), 0) + 1
+#             c += 1
+#
+#     for word, count in sorted(counter.items(), key=lambda kv: kv[1], reverse=True):
+#         print(f'{word} -> {count}')
+#
+#
+# policz_slowa('pan-tadeusz.txt')
+#
 
 
 # ## 6. Przetwarzanie plików
@@ -83,18 +83,16 @@ with open('zawodnicy.csv', 'r', newline='', encoding="utf8") as plik_csv:
     zawodnicy = []
     waga = []
     wzrost = []
-    kraj = set()
-    lista_z = dict()
+    kraje = set()
+    sl_z = dict()
     sl_sr_wz = dict()
     lista_wz = []
     for row in read_csv:
         zawodnicy.append(row)
         waga.append(row[5])
         wzrost.append(row[4])
-        kraj.add(row[2])
-        lista_z[row[2]] = lista_z.get(row[2], 0) + 1
-        # sl_sr_wz[row[2]] = [sl_sr_wz.get(row[2], 0) + int(row[4]), sl_sr_wz.get(row[2], 0) + 1]
-        # sl_sr_wz[row[2]] = sl_sr_wz.get(row[2], 0) + [200, 1]
+        kraje.add(row[2])
+        sl_z[row[2]] = sl_z.get(row[2], 0) + 1
         tmp_wz = [row[2], row[4]]
         lista_wz.append(tmp_wz)
 # 1
@@ -108,21 +106,18 @@ print(f'najniższy: {list(row[0] + " " + row[1] for row in zawodnicy if row[4] =
 # print(f'waga wszystkich {kraj_z}: {sum(list(int(row[5]) for row in zawodnicy if row[2] == kraj_z))}')
 
 # 3
-for kraj, liczba in sorted(lista_z.items(), key=lambda item: item[1], reverse=True):
-    print(f'{kraj} - {liczba}')
+for _kraj, liczba in sorted(sl_z.items(), key=lambda item: item[1], reverse=True):
+    print(f'{_kraj} - {liczba}')
 
 # 4
 
 print(lista_wz)
 test = len(list(filter(lambda a: a[0] == 'POL', lista_wz)))
-test1 = sum(list(filter(lambda i: int(i[1]) if i[0] == 'POL' else None, lista_wz)))
 print(test)
-print(test1)
 
+print(kraje)
+for i in kraje:
+    print(i, len(list(filter(lambda a: a[0] == i, lista_wz))), )
 
-
-
-# print(sl_sr_wz)
-# print(lista_z)
-# tmp = list(k, lista_z[k], sl_sr_wz[k] / int(lista_z[k]) for k in sl_sr_wz)
-# print(tmp)
+for kraj in kraje:
+    print(kraj, )
